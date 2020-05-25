ff0349d4c1849d1f52401d4d1c60a9b9ceb18bb3
moh2099 <moh_2323@hotmail.com>
2020-05-25 19:14:16 +0300

    asd

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import DeckListView from './screens/DeckListView'
+import IndividualDeckView from './screens/IndividualDeckView'
+import NewDeckView from './screens/NewDeckView'
+import NewQuestionView from './screens/NewQuestionView'
+import QuizView from './screens/QuizView'
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <DeckListView />
+      {/* <IndividualDeckView />
+      <QuizView />
+      <NewQuestionView />
+      <NewDeckView /> */}
     </View>
+
   );
 }
 
---
+++ screens/DeckListView.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+// import DeckListView from './screens/DeckListView'
+// import IndividualDeckView from './screens/IndividualDeckView'
+// import NewDeckView from './screens/NewDeckView'
+// import NewQuestionView from './screens/NewQuestionView'
+// import QuizView from './screens/QuizView'
+export default function DeckListView() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+}
+  
+  
